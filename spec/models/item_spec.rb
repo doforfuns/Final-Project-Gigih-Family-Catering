8876b5791e120456e232f3df155c8e2a589900d4
Saeful Sultan <saefulsultan@gmail.com>
2022-04-23 14:15:46 +0000

    Add validation foration for Item, is valid with a name and a price, is invalid without a name, is invalid without a price, is invalid with a duplicate name

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,29 @@ RSpec.describe Item, type: :model do
   it 'has a valid factory' do
     expect(FactoryBot.build(:item)).to be_valid
   end
+
+  it 'is valid with a name and a price' do
+    expect(FactoryBot.build(:item)).to be_valid
+  end
+
+  it 'is invalid without a name' do
+    item = FactoryBot.build(:item, name: nil)
+
+    item.valid?
+
+    expect(item.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a price' do
+    item = FactoryBot.build(:item, price: nil)
+    item.valid?
+    expect(item.errors[:price]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    item1 = FactoryBot.create(:item, name: 'Nasi Uduk')
+    item2 = FactoryBot.build(:item, name: 'Nasi Uduk')
+    item2.valid?
+    expect(item2.errors[:name]).to include("has already been taken")
+  end
 end
