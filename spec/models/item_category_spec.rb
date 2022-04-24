6ee433150a7ad57ab3f779c01b0cb9b0ab7e9c2b
Saeful Sultan <saefulsultan@gmail.com>
2022-04-24 17:17:31 +0000

    Add customary spec for ItemCategory model, invalid without belongs_to association to Item and invalid without belongs_to association to Category

--- spec/models/item_category_spec.rb
+++ spec/models/item_category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ItemCategory, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without belongs_to association to Item' do
+    item_category = ItemCategory.reflect_on_association(:items).macro
+    expect(item_category).to eq(:belongs_to)
+  end
+
+  it 'is invalid without belongs_to association to Category' do
+    item_category = ItemCategory.reflect_on_association(:categories).macro
+    expect(item_category).to eq(:belongs_to)
+  end
 end
