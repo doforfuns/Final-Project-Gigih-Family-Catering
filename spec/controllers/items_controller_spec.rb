7fecdce686d565014ae82da5b394497679c7aa66
Saeful Sultan <saefulsultan@gmail.com>
2022-04-24 17:54:58 +0000

    Add spec for index, show, new, and edit controller of Item

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,4 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController do
+  describe 'GET #index' do
+    it "populates an array of all items" do 
+      nasi_uduk = create(:item, name: "Nasi Uduk")
+      kerak_telor = create(:item, name: "Kelar Telor")
+      get :index
+      expect(assigns(:items)).to match_array([nasi_uduk, kerak_telor])
+    end
+
+    it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
+
+  describe 'GET #show' do
+    it "assigns the requested item to @item" do
+      item = create(:item)
+      get :show, params: { id: item }
+      expect(assigns(:item)).to eq item
+    end
+    it "renders the :show template" do
+      item = create(:item)
+      get :show, params: { id: item }
+      expect(response).to render_template :show
+    end
+  end
+
+  describe 'GET #new' do
+    it "assigns a new Item to @item" do
+      get :new
+      expect(assigns(:item)).to be_a_new(Item)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(:response).to render_template :new
+    end
+  end
+
+  describe 'GET #edit' do
+    it "assigns the requested item to @item" do
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(assigns(:item)).to eq item
+    end
+
+    it "renders the :edit template" do
+      item = create(:item)
+      get :edit, params: { id: item }
+      expect(response).to render_template :edit
+    end
+  end
 end
