40dcdd6070cf9dec4c1435a364dd39c6166116cf
Saeful Sultan <saefulsultan@gmail.com>
2022-04-24 13:21:12 +0000

    Add validation for Category, invalid with a duplicate name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_23_124527) do
+ActiveRecord::Schema.define(version: 2022_04_24_124904) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
